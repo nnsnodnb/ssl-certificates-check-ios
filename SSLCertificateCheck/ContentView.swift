091768cd31be3237afc847645d6e148fe02a4246
nnsnodnb <nnsnodnb@gmail.com>
2023-10-12 22:58:58 +0900

    Initial commit

---
+++ SSLCertificateCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSLCertificateCheck
+//
+//  Created by Yuya Oka on 2023/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SSLCertificateCheck/SSLCertificateCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSLCertificateCheckApp.swift
+//  SSLCertificateCheck
+//
+//  Created by Yuya Oka on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct SSLCertificateCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
