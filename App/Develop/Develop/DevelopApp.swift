3f3bb504d7957d7f2e93c2fc347d30d97ba198f2
nnsnodnb <nnsnodnb@gmail.com>
2026-02-27 12:04:00 +0900

    Remove AppDelegate

--- App/Develop/Develop/DevelopApp.swift
+++ App/Develop/Develop/DevelopApp.swift
@@ -6,14 +6,13 @@
 //
 
 import Application
+import class FirebaseCore.FirebaseApp
+import class GoogleMobileAds.MobileAds
+import class RevenueCat.Purchases
 import SwiftUI
 
 @main
 struct DevelopApp: App {
-  // MARK: - Properties
-  @UIApplicationDelegateAdaptor(AppDelegate.self)
-  var delegate
-  
   // MARK: - Body
   var body: some Scene {
     WindowGroup {
@@ -23,6 +22,21 @@ struct DevelopApp: App {
           searchPageBottomBannerAdUnitID: "ca-app-pub-3940256099942544/2435281174",
         )
       )
+    }
+  }
+
+  // MARK: - Initialize
+  init() {
+    FirebaseApp.configure()
+    Task {
+      _ = await MobileAds.shared.start()
+      MobileAds.shared.requestConfiguration.testDeviceIdentifiers = [
+        "F8BB1C28-BAE8-11D6-9C31-00039315CD46",
+      ]
+    }
+    Purchases.configure(withAPIKey: "appl_tCBoNHVYLrNNHLlPSrarLoDORLz")
+    Task {
+      _ = try await Purchases.shared.logIn("$RCAnonymousID:ccff33d798344877aa1f363be90eb38f")
     }
   }
 }
